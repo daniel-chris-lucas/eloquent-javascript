1f0f5124d2fd345263a9be4ab4dd06e00f2b4039
Daniel Lucas <daniel.chris.lucas@gmail.com>
2015-06-28 19:31:58 +0200

    Added functions to language

--- chapter11/language.js
+++ chapter11/language.js
@@ -201,3 +201,35 @@ function run() {
 
     return evaluate(parse(program), env);
 }
+
+/**
+ * Functions.
+ */
+specialForms["fun"] = function (args, env) {
+    if (!args.length) {
+        throw new SyntaxError("Functions need a body");
+    }
+
+    function name (expr) {
+        if (expr.type != "word") {
+            throw new SyntaxError("Arg names must be words");
+        }
+        return expr.name;
+    }
+
+    var argNames = args.slice(0, args.length - 1).map(name),
+        body     = args[args.length - 1];
+
+    return function () {
+        if (arguments.length != argNames.length) {
+            throw new TypeError("Wrong number of arguments");
+        }
+
+        var localEnv = Object.create(env);
+        for (var i = 0; i < arguments.length; i++) {
+            localEnv[argNames[i]] = arguments[i];
+        }
+
+        return evaluate(body, localEnv);
+    };
+};
