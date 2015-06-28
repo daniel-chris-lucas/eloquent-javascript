7e85fe9830da66499765d87bd30770c1926e94b9
Daniel Lucas <daniel.chris.lucas@gmail.com>
2015-06-28 19:40:52 +0200

    Added arrays to language

--- chapter11/language.js
+++ chapter11/language.js
@@ -233,3 +233,18 @@ specialForms["fun"] = function (args, env) {
         return evaluate(body, localEnv);
     };
 };
+
+/**
+ * Array
+ */
+topEnv["array"] = function () {
+    return Array.prototype.slice.call(arguments, 0);
+};
+
+topEnv["length"] = function (array) {
+    return array.length;
+};
+
+topEnv["element"] = function (array, i) {
+    return array[i];
+};
