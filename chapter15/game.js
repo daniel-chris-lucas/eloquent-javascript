ffdda59f4ed2b5f48e93e6735484e9253e6a1182
Daniel Lucas <daniel.chris.lucas@gmail.com>
2015-06-29 23:18:17 +0200

    Added obstacleAt and actorAt methods to the Level class

--- chapter15/game.js
+++ chapter15/game.js
@@ -342,3 +342,55 @@ DOMDisplay.prototype.scrollPlayerIntoView = function () {
 DOMDisplay.prototype.clear = function () {
     this.wrap.parentNode.removeChild(this.wrap);
 };
+
+/**
+ * Detect if there is an obstacle at the given coordiantes.
+ *
+ * @param  {Vector} pos  The coordinates of the point.
+ * @param  {Integer} size The size of the point.
+ * @return {mixed|String}  The type of object at the point if there is something. 
+ */
+Level.prototype.obstacleAt = function (pos, size) {
+    var xStart = Math.floor(pos.x),
+        xEnd   = Math.ceil(pos.x + size.x),
+        yStart = Math.floor(pos.y),
+        yEnd   = Math.ceil(pos.y + size.y);
+
+    if (xStart < 0 || xEnd > this.width || yStart < 0) {
+        return "wall";
+    }
+
+    if (yEnd > this.height) {
+        return "lava";
+    }
+
+    for (var y = yStart; y < yEnd; y++) {
+        for (var x = xStart; x < xEnd; x++) {
+            var fieldType = this.grid[y][x];
+            if (fieldType) {
+                return fieldType;
+            }
+        }
+    }
+};
+
+/**
+ * Check if there is an actor overlapping the position of the argument.
+ * @param  {Object} actor The actor to check
+ * @return {mixed|null}   The actor that os overlapping if there is one.
+ */
+Level.prototype.actorAt = function (actor) {
+    for (var i = 0; i < this.actors.length; i++) {
+        var other = this.actors[i];
+
+        if (
+            other != actor && 
+            actor.pos.x + actor.size.x > other.pos.x &&
+            actor.pos.x < other.pos.x + other.size.x &&
+            actor.pos.y + actor.size.y > other.pos.y &&
+            actor.pos.y < other.pos.y + other.size.y
+        ) {
+            return other;
+        }
+    }
+};
