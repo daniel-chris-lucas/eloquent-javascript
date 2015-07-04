b294ef5e67382d99721b2408bd289fa45c5168a2
Daniel Lucas <daniel.chris.lucas@gmail.com>
2015-07-04 22:19:01 +0200

    Done Content Negotiation Again exercise

---
+++ chapter20/content-negotiation.js
@@ -0,0 +1,51 @@
+/**
+ * Content negotiation, again
+ *
+ * 
+ * In Chapter 17, the first exercise was to make several requests to <br>
+ * eloquent-javascript.net/author, asking for different types of content <br>
+ * by passing different Accept headers.
+ *
+ * Do this again, using Node’s http.request function. Ask for at least the <br>
+ * media types text/plain, text/html, and application/json. Remember that <br>
+ * headers to a request can be given as an object, in the headers property <br>
+ * of http.request’s first argument.
+ *
+ * Write out the content of the responses to each request.
+ */
+var http = require("http");
+
+function readStreamAsString (stream, callback) {
+    var content = "";
+    stream.on("data", function (chunk) {
+        content += chunk;
+    });
+    stream.on("end", function () {
+        callback(null, content);
+    });
+    stream.on("error", function (error) {
+        callback(error);
+    });
+}
+
+fileTypes = [
+    "text/plain",
+    "text/html",
+    "application/json"
+];
+
+fileTypes.forEach(function (type) {
+    http.request({
+        hostname: "eloquentjavascript.net",
+        path: "/author",
+        headers: {Accept: type}
+    }, function (response) {
+        readStreamAsString(response, function (error, content) {
+            if (error) {
+                throw error;
+            }
+
+            console.log("Type " + type + ": " + content);
+        });
+    }).end();
+});
