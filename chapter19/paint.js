4c5d98716c0f466ac433c1d05a300347c28edfe6
Daniel Lucas <daniel.chris.lucas@gmail.com>
2015-07-04 18:12:34 +0200

    Added color picker tool

--- chapter19/paint.js
+++ chapter19/paint.js
@@ -398,3 +398,25 @@ tools.Rectangle = function (event, cx) {
         document.body.removeChild(trackingNode);
     });
 };
+
+function colorAt (cx, x, y) {
+    var pixel = cx.getImageData(x, y, 1, 1).data;
+    return "rgb(" + pixel[0] + ", " + pixel[1] + ", " + pixel[2] + ")";
+}
+
+tools["Pick color"] = function (event, cx) {
+    var pos = relativePos(event, cx.canvas);
+    try {
+        var color = colorAt(cx, pos.x, pos.y);
+    } catch (e) {
+        if (e instanceof SecurityError) {
+            alert("Unable to access your picture's pixel data");
+            return;
+        } else {
+            throw e;
+        }
+    }
+
+    cx.fillStyle = color;
+    cx.strokeStyle = color;
+};
